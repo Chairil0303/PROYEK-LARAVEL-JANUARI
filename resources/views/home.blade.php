cc56b61299264547febba9da96a2f68e9ab482e7
chairil0303 <chairilsyahrain24@gmail.com>
2025-01-24 06:28:48 +0700

    add home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,13 +3,13 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Halaman about</title>
+    <title>My Proyek</title>
 </head>
 <body>
     <a href="">home</a>
     <a href="about">about</a>
     <a href="">contact</a>
 
-    <h1>Halaman About</h1>
+    <h1>Home</h1>
 </body>
 </html>
