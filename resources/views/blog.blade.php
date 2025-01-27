b07b8f695fbaca5537f4b40629d22db8204c69bf
chairil0303 <chairilsyahrain24@gmail.com>
2025-01-27 09:21:25 +0700

    membuat ui artikel

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,4 +1,26 @@
 <x-layout>
     <x-slot:title>{{$title}}</x-slot:title>
     <h3 class="text-xl">Welcome to my blog</h3>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 ">Judul Artikel 1
+        </h2>
+        <div class="text-base text-gray-500">
+            <a href="">Chairil Syahrain
+            </a> | 26 Januari 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora reiciendis totam unde dolore, minus obcaecati est pariatur nemo minima explicabo. Omnis, delectus?</p>
+        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo </a>
+    </article>
+
+    <article class="py-8 max-w-screen-md border-b border-gray-300">
+        <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 ">Judul Artikel 2
+        </h2>
+        <div class="text-base text-gray-500">
+            <a href="">Chairil Syahrain
+            </a> | 20 Januari 2025
+        </div>
+        <p class="my-4 font-light">Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, possimus totam.</p>
+        <a href="" class="font-medium text-blue-500 hover:underline">Read more &raquo </a>
+    </article>
 </x-layout>
