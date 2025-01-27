6dc7dbba320ffbafd09bd5efa3b249b133c75c9b
chairil0303 <chairilsyahrain24@gmail.com>
2025-01-27 09:08:56 +0700

    mobile nav link fix

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -9,10 +9,10 @@
             <div class="hidden md:block">
                 <div class="ml-10 flex items-baseline space-x-4">
                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-                <a href="/" class="{{request()->is('/') ? 'bg-gray-900 text-white' :  'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium  text-white" aria-current="page">Home</a>
-                <a href="/blog" class="{{request()->is('blog') ? ' bg-gray-900 text-white' :  'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md  px-3 py-2 text-sm font-medium ">Blog</a>
-                <a href="/about" class="{{request()->is('about') ? ' bg-gray-900 text-white' :  'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium ">About</a>
-                <a href="/contact" class="{{request()->is('contact') ? 'bg-gray-900 text-white' :  'text-gray-300 hover:bg-gray-700 hover:text-white' }} rounded-md px-3 py-2 text-sm font-medium ">Contact</a>
+                <x-navlink href="/" :active="request()->is('/')">Home</x-navlink>
+                <x-navlink href="/blog" :active="request()->is('blog')">Blog</x-navlink>
+                <x-navlink href="/about" :active="request()->is('about')">About</x-navlink>
+                <x-navlink href="/contact" :active="request()->is('contact')" >Contact</x-navlink>
                 </div>
             </div>
             </div>
@@ -85,9 +85,9 @@
         <div class="space-y-1 px-2 pt-2 pb-3 sm:px-3">
             <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
             <a href="#" class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Home</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
-            <a href="#" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
+            <a href="/blog" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Blog</a>
+            <a href="/about" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">About</a>
+            <a href="/contact" class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Contact</a>
         </div>
         <div class="border-t border-gray-700 pt-4 pb-3">
             <div class="flex items-center px-5">
