45de8282088c6097a98eba391d355b21fb518d04
chairil0303 <chairilsyahrain24@gmail.com>
2025-01-27 10:22:25 +0700

    membuat halaman single post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,20 +1,16 @@
 <x-layout>
     <x-slot:title>{{$title}}</x-slot:title>
-    <h3 class="text-xl">Welcome to my blog</h3>
 
-    @foreach($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
-        <a href="posts/{{$post['id']}}" class="hover:underline">
+        
             <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900 ">{{$post['title']}}
-            </h2>
-        </a>
+            </h2> 
             <div class="text-base text-gray-500">
                 <a href="">{{$post['author']}}
                     </a> | 26 Januari 2025
                 </div>
-        <p class="my-4 font-light">{{Str::limit($post['body'],100 )}}</p>
-        <a href="posts/{{$post['id']}}" class="font-medium text-blue-500 hover:underline">Read more &raquo </a>
+        <p class="my-4 font-light">{{$post['body'], }}</p>
+        <a href="/posts" class="font-medium text-blue-500 hover:underline">Back to posts &laquo </a>
     </article>
-    @endforeach
     
 </x-layout>
--- routes/web.php
+++ routes/web.php
@@ -25,11 +25,13 @@ Route::get('/about', function () {
 Route::get('/posts', function () {
     return view('posts', ['title'=>'Blog', 'posts' => [
         [
+            'id' => '1',
             'title' => 'Judul artikel 1',
             'author' => 'Chairil syahrain',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, possimus totam.'
         ],
         [
+            'id' => '2',
             'title' => 'Judul artikel 2',
             'author' => 'Chairil syahrain',
             'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora Lorem ipsum dolor sit amet.'
@@ -38,6 +40,35 @@ Route::get('/posts', function () {
 ]);
 });
 
+Route::get('/posts/{id}', function ($id) {
+    // return view('contact',['title'=>'contact']);
+    $posts = [
+        [
+            'id' => '1',
+            'title' => 'Judul artikel 1',
+            'author' => 'Chairil syahrain',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, possimus totam.'
+        ],
+        [
+            'id' => '2',
+            'title' => 'Judul artikel 2',
+            'author' => 'Chairil syahrain',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora Lorem ipsum dolor sit amet.'
+        ]
+    ];
+    
+    $post = Arr::first($posts, function($post) use($id){
+        return $post['id'] == $id ;
+    });
+
+    
+    return view('post', ['title' => 'Single post', 'post' => $post]);
+
+}
+);
+
 Route::get('/contact', function () {
     return view('contact',['title'=>'contact']);
 });
+
+
