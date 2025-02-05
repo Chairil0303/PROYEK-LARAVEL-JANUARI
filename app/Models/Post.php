afed177059704379f365a39d2d5da8b701558dbb
chairil0303 <chairilsyahrain24@gmail.com>
2025-02-05 10:22:10 +0700

    laravel 11. 9 Olequent ORM selesai

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,12 +1,12 @@
-<?php 
+<?php
 
 namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-
-
 
 class Post extends Model
 {
-};
-
-?>
+    use HasFactory;
+    protected $fillable = ['title','author','slug','body'];
+}
--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,6 +1,8 @@
 <x-layout>
     <x-slot:title>{{$title}}</x-slot:title>
     <h3 class="text-xl">Welcome to my blog</h3>
+    
+
 
     @foreach($posts as $post)
     <article class="py-8 max-w-screen-md border-b border-gray-300">
@@ -10,7 +12,7 @@
         </a> 
             <div class="text-base text-gray-500">
                 <a href="">{{$post['author']}}
-                    </a> | 26 Januari 2025
+                    </a> | {{$post->created_at->format('j F Y')}}
                 </div>
         <p class="my-4 font-light">{{Str::limit($post['body'],100 )}}</p>
         <a href="posts/{{$post['slug']}}" class="font-medium text-blue-500 hover:underline">Read more &raquo </a>
