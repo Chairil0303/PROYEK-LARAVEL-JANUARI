127860620b6554756007852f649f6daea252a677
chairil0303 <chairilsyahrain24@gmail.com>
2025-02-05 09:33:19 +0700

    membuat model extends, dan menghapus model vanila

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -1,44 +1,14 @@
 <?php 
 
 namespace App\Models;
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
 
 
-class Post 
+class Post extends Model
 {
-    public static function all()
-    {
-        return [
-        [
-            'id' => '1',
-            'slug' => 'judul-artikel-1',
-            'title' => 'Judul artikel 1',
-            'author' => 'Chairil syahrain',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi, possimus totam.'
-        ],
-        [
-            'id' => '2',
-            'slug' => 'judul-artikel-2',
-            'title' => 'Judul artikel 2',
-            'author' => 'Chairil syahrain',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt dolor autem, tempora Lorem ipsum dolor sit amet.'
-        ]
-    ] ;
-    }
+    
 
-    public static function find($slug):array
-    {
-        $post = Arr::first(static::all(), function($post) use($slug){
-        return $post['slug'] == $slug ;
-    });
-
-    if(! $post ){
-        abort(404);
-    };
-
-    return $post;
-    }
-}
+};
 
 ?>
