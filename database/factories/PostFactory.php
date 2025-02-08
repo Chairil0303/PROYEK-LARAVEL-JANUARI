4c4f87091b2963a1cb229bfd85e97e2df21511ef
chairil0303 <chairilsyahrain24@gmail.com>
2025-02-09 01:05:47 +0700

    eloquent relationship 1

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,8 +2,10 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Str;
+
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
@@ -20,7 +22,7 @@ class PostFactory extends Factory
         return [
             //
             'title'=>fake()->sentence(),
-            'author'=>fake()->name(),
+            'author_id'=>User::factory(),
             'slug' =>Str::slug(fake()->sentence()),
             'body'=>fake()->text()
         ];
