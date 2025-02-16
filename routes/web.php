361cd6d8cf8bf40c1a1a503fe1973adb0f084280
chairil0303 <chairilsyahrain24@gmail.com>
2025-02-16 17:26:05 +0700

    eager loading from routes

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::get('/about', function () {
 
 Route::get('/posts', function () {
 
+    // eager loading dengan with
     $posts = Post::with(['author','category'])->latest()->get();
 
     return view('posts', ['title'=>'Blog', 'posts' => $posts ]);
@@ -39,12 +40,18 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 );
 
 Route::get('/authors/{user:username}', function (User $user) {
-    return view('posts', ['title' => count($user->posts).' Artikels By ' . $user->name , 'posts' => $user->posts]);
+
+    $posts = $user->posts->load('category','author'); 
+
+    return view('posts', ['title' => count($user->posts).' Artikels By ' . $user->name , 'posts' => $posts]);
 }
 );
 
 Route::get('/categories/{category:slug}', function (Category $category) {
-    return view('posts', ['title' => 'Articles in : '. $category->name, 'posts' => $category->posts]);
+
+    $posts = $category->posts->load('category','author'); 
+
+    return view('posts', ['title' => 'Articles in : '. $category->name, 'posts' => $posts]);
 }
 );
 
